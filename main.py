0c1b011e10e8793824f5c4c27231129304c45378
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-26 00:08:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def double(number):
+  return (number * 2)
+
+new_number = double(5)
+
+print (new_number)
+
+def uppercase(text):
+  return text.upper()
+
+print(uppercase("nick"))
+
+names = ["nick, Jane, sara"]
+
+for name in names:
+  print (uppercase(name))
